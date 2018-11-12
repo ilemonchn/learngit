92161e52fd87e939f3d07a9f59160988b90cf072
chenmeng <chenmeng@sellerpro.com>
2018-11-12 16:33:40 +0800

    Lemon POJO

---
+++ src/main/java/com/lemon/tag/Lemon.java
@@ -0,0 +1,25 @@
+package com.lemon.tag;
+
+/**
+ * Created by Lemon on 2018/11/12.
+ */
+public class Lemon {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
