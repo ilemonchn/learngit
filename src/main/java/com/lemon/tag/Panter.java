f6200bc18b94f64c48090986637d28b5dcdf209e
chenmeng <chenmeng@sellerpro.com>
2018-11-12 16:52:48 +0800

    2.0

---
+++ src/main/java/com/lemon/tag/Panter.java
@@ -0,0 +1,20 @@
+package com.lemon.tag;
+
+
+
+/**
+ * Created by Lemon on 2018/11/12.
+ */
+public class Panter {
+
+    public static Lemon lemon = new Lemon();
+
+    public static void pant(){
+        lemon.setName("lemon");
+        lemon.setAge(20);
+        System.out.println(lemon.getName());
+    }
+
+
+
+}
